1ca76ae582c4f79a652f1a22e4452834a8cbd8b8
Bartosz Lenart <lenart.consulting@gmail.com>
2024-01-10 23:25:16 +0100

    17: Cleanup and code formatting. (#18)

--- src/views/text_box.c
+++ src/views/text_box.c
@@ -20,8 +20,7 @@ DrawTextBoxedSelectable(Font font, const char *text, Rectangle rec, float fontSi
     int endLine = -1;           // Index where to stop drawing (where a line ends)
     int lastk = -1;             // Holds last value of the character position
 
-    for (int i = 0, k = 0; i < length; i++, k++)
-    {
+    for (int i = 0, k = 0; i < length; i++, k++) {
         // Get next codepoint from byte string and glyph index in font
         int codepointByteCount = 0;
         int codepoint = GetCodepoint(&text[i], &codepointByteCount);
@@ -33,8 +32,7 @@ DrawTextBoxedSelectable(Font font, const char *text, Rectangle rec, float fontSi
         i += (codepointByteCount - 1);
 
         float glyphWidth = 0;
-        if (codepoint != '\n')
-        {
+        if (codepoint != '\n') {
             glyphWidth = (font.glyphs[index].advanceX == 0) ? font.recs[index].width*scaleFactor : font.glyphs[index].advanceX*scaleFactor;
 
             if (i + 1 < length) glyphWidth = glyphWidth + spacing;
@@ -45,29 +43,25 @@ DrawTextBoxedSelectable(Font font, const char *text, Rectangle rec, float fontSi
         // and change states again and again recursively until the end of the text (or until we get outside of the container).
         // When wordWrap is OFF we don't need the measure state so we go to the drawing state immediately
         // and begin drawing on the next line before we can get outside the container.
-        if (state == MEASURE_STATE)
-        {
+        if (state == MEASURE_STATE) {
             // TODO: There are multiple types of spaces in UNICODE, maybe it's a good idea to add support for more
             // Ref: http://jkorpela.fi/chars/spaces.html
             if ((codepoint == ' ') || (codepoint == '\t') || (codepoint == '\n')) endLine = i;
 
-            if ((textOffsetX + glyphWidth) > rec.width)
-            {
+            if ((textOffsetX + glyphWidth) > rec.width) {
                 endLine = (endLine < 1)? i : endLine;
                 if (i == endLine) endLine -= codepointByteCount;
                 if ((startLine + codepointByteCount) == endLine) endLine = (i - codepointByteCount);
 
                 state = !state;
             }
-            else if ((i + 1) == length)
-            {
+            else if ((i + 1) == length) {
                 endLine = i;
                 state = !state;
             }
             else if (codepoint == '\n') state = !state;
 
-            if (state == DRAW_STATE)
-            {
+            if (state == DRAW_STATE) {
                 textOffsetX = 0;
                 i = startLine;
                 glyphWidth = 0;
@@ -77,21 +71,14 @@ DrawTextBoxedSelectable(Font font, const char *text, Rectangle rec, float fontSi
                 lastk = k - 1;
                 k = tmp;
             }
-        }
-        else
-        {
-            if (codepoint == '\n')
-            {
-                if (!wordWrap)
-                {
+        } else {
+            if (codepoint == '\n') {
+                if (!wordWrap){
                     textOffsetY += (font.baseSize + font.baseSize/2)*scaleFactor;
                     textOffsetX = 0;
                 }
-            }
-            else
-            {
-                if (!wordWrap && ((textOffsetX + glyphWidth) > rec.width))
-                {
+            } else {
+                if (!wordWrap && ((textOffsetX + glyphWidth) > rec.width)) {
                     textOffsetY += (font.baseSize + font.baseSize/2)*scaleFactor;
                     textOffsetX = 0;
                 }
@@ -101,21 +88,18 @@ DrawTextBoxedSelectable(Font font, const char *text, Rectangle rec, float fontSi
 
                 // Draw selection background
                 bool isGlyphSelected = false;
-                if ((selectStart >= 0) && (k >= selectStart) && (k < (selectStart + selectLength)))
-                {
+                if ((selectStart >= 0) && (k >= selectStart) && (k < (selectStart + selectLength))) {
                     DrawRectangleRec((Rectangle){ rec.x + textOffsetX - 1, rec.y + textOffsetY, glyphWidth, (float)font.baseSize*scaleFactor }, selectBackTint);
                     isGlyphSelected = true;
                 }
 
                 // Draw current character glyph
-                if ((codepoint != ' ') && (codepoint != '\t'))
-                {
+                if ((codepoint != ' ') && (codepoint != '\t')) {
                     DrawTextCodepoint(font, codepoint, (Vector2){ rec.x + textOffsetX, rec.y + textOffsetY }, fontSize, isGlyphSelected? selectTint : tint);
                 }
             }
 
-            if (wordWrap && (i == endLine))
-            {
+            if (wordWrap && (i == endLine)) {
                 textOffsetY += (font.baseSize + font.baseSize/2)*scaleFactor;
                 textOffsetX = 0;
                 startLine = endLine;
@@ -123,7 +107,6 @@ DrawTextBoxedSelectable(Font font, const char *text, Rectangle rec, float fontSi
                 glyphWidth = 0;
                 selectStart += lastk - k;
                 k = lastk;
-
                 state = !state;
             }
         }
